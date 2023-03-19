1760f98607aa3add01499bec460e31ce4c1b071f
DanubiaM <dubamacedoo@gmail.com>
2023-03-19 00:10:18 -0400

    added useState to get values of form

--- manager-app/src/components/Form.js
+++ manager-app/src/components/Form.js
@@ -1,9 +1,19 @@
+import {useState} from 'react'
+
 function Form(){
 
     function cadastrar(e){
         e.preventDefault()
+        console.log(`Usuario ${usuario} com senha ${password} nascido ${born} com nome ${name}`)
         console.log("Enviando to ")
     }
+
+    const [name, setName] = useState()
+    const [usuario, setUsuario] = useState("DanubiaM")
+    const [born, setBorn] = useState()
+    const [password, setPassword] = useState()
+
+
 
     return (
 
@@ -11,10 +21,27 @@ function Form(){
             <p>We go talk ? Please, insert your info</p>
 
             <form onSubmit={cadastrar}>
-                <label>Inser your name</label>
-                <input type="text" placeholder="Name"/> 
-                <label>Your born</label>
-                <input type="date" />
+                <div>
+                    <label htmoFor="name"> Inser your name</label>
+                    <input type="text" name="name" id="name" placeholder="Name" onChange={(e) => setName(e.target.value)}/> 
+                </div>
+                <div>
+                    <label htmlFor="born">Your born</label>
+                    <input type="date" name="born" id="born" onChange={(e) => setBorn(e.target.value)}/>
+                </div>
+                <br/>
+                <div>
+                    <label htmoFor="user"> Inser your user</label>
+                    <input type="text" name="user" id="user" placeholder="user" value={usuario} onChange={(t)=> setUsuario(t.target.value)}/> 
+                </div>
+                <div>
+                    <label htmlFor="password">Your password</label>
+                    <input type="password" name="password" id="password" onChange={(e) => setPassword(e.target.value)}/>
+                </div>
+             
+
+
+
 
                 <button type="submit">Send</button>
             
