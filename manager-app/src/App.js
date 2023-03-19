2aab5f72240eda683206505e57d6f86cc49cb573
DanubiaM <dubamacedoo@gmail.com>
2023-03-18 22:25:36 -0400

    learning how use css in a specify component

--- manager-app/src/App.js
+++ manager-app/src/App.js
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <div className="App">
-      <Card/>     
+      <Card/>   
     </div>
   );
 }
--- manager-app/src/components/Card.js
+++ manager-app/src/components/Card.js
@@ -1,15 +1,16 @@
 import SayMyName from "./SayMyName";
 import Pessoa from "./Pessoa";
+import styles from "./Card.module.css"
+
 function Card(){
     const nome = "John Wick"
     const profession = "Actor"
     const url="https://m.extra.globo.com/incoming/25674117-f59-dcd/w488h275-PROP/whatsapp-image-2023-03-12-at-09.43.08.jpeg.jpg"
 
     return (
-        <div>
+        <div className={styles.card}>
             <h1>I'm use React Framework </h1>
-            <div>
-                
+            <div className={styles.body}>                
                 <Pessoa 
                  name={nome}
                  profession={profession} 
