2f35f91935489e6ec429a93d3ed4b08e271fe59c
DanubiaM <dubamacedoo@gmail.com>
2023-04-07 21:58:45 -0400

    feat: added consume API in select component

--- costs/src/components/project/ProjectForm.js
+++ costs/src/components/project/ProjectForm.js
@@ -1,9 +1,28 @@
-import styles from './ProjectForm.module.css'
+import {useEffect, useState} from 'react';
+import styles from './ProjectForm.module.css';
 import Input from '../form/Input.js';
 import Select  from '../form/Select';
 import SubmitButton from '../form/SubmitButton';
 
 function ProjectForm({btnText}){
+
+    const [categories, setCategories] = useState([])
+
+    useEffect(() =>{
+        fetch('http://localhost:5000/categories',{
+            method: 'GET',
+            headers:{
+                'Content-Type':'application/json',
+            }
+        })
+        .then((resp) => resp.json())
+        .then((data) =>{
+            setCategories(data)
+        })
+        .catch((err) => console.log(err))
+    }, [])
+
+
     return (
         <form className={styles.form}> 
             <div>
@@ -22,7 +41,7 @@ function ProjectForm({btnText}){
                     placeholder="Insira o orçamento total"
                 />
             </div>
-            <Select name="category_id" text="Selecione a categoria"/>
+            <Select name="category_id" text="Selecione a categoria" options={categories}/>
             <SubmitButton text={btnText}/>
         </form>
     );
