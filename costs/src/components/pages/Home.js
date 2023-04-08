1f84772366a952d7dee6fda23eda68ef4c0304b4
DanubiaM <dubamacedoo@gmail.com>
2023-04-07 20:20:23 -0400

    feat: make page home with stylization

--- costs/src/components/pages/Home.js
+++ costs/src/components/pages/Home.js
@@ -1,8 +1,9 @@
 import savings from "../../img/savings.svg"
+import styles from "./Home.module.css"
 
 function Home(){
     return (
-        <section>
+        <section className={styles.home_container}>
             <h1>Bem-vindo ao <span>Coats</span>!</h1>
             <p>Gerencie seus projetos aqui.</p>
             <a href="/"> Criar Projeto</a>
